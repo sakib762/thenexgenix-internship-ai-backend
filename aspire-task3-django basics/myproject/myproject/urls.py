e3065089c373523898dfd4ae650b46af0899b5ae
MD Sakibur Rahaman <hackerboss012@gmail.com>
2025-07-28 12:50:36 +0600

    completing myproject

--- aspire-task3-django basics/myproject/myproject/urls.py
+++ aspire-task3-django basics/myproject/myproject/urls.py
@@ -2,7 +2,7 @@
 URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,12 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from . import views
+from django.conf.urls.static import static 
+from django.conf import settings 
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
-    path('', views.homepage ),
-    path("about/", views.about),
-    path("posts/", include('posts.urls'))
+    path('', views.homepage),
+    path('about/', views.about),
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- aspire-task3-django basics/myproject/myproject/views.py
+++ aspire-task3-django basics/myproject/myproject/views.py
@@ -1,9 +1,12 @@
-#from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
-    #return HttpResponse("Hello World,Im Home")
-    return render(request, "home.html")
+    # return HttpResponse("Hello World! I'm Home.")
+    return render(request, 'home.html')
+
+
 def about(request):
-    #return HttpResponse("My About page")
-    return render(request, "about.html")
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')
